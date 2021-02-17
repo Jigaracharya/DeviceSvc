7e6bb5793c334de145dd83a1867ecdee7b0a53a2
Jigaracharya <jigaracharya@gmail.com>
2021-02-16 18:30:50 -0600

    controller added

---
+++ src/DeviceSvc/DeviceSvc.API/Controllers/DevicesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeviceSvc.API.Controllers
+{
+    public class DevicesController : Controller
+    {
+        // GET: DevicesController
+        public ActionResult Get()
+        {
+            return Ok(new { Name = "Jigar Acharya" });
+        }
+
+    }
+}
